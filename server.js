e392aae1706d492e1d5623e2e287bdffea730d21
abhay <thisisabhaypratapsingh@gmail.com>
2025-03-30 13:33:01 +0530

    Auth done

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/auth', authRoutes);
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// Connect Database
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
